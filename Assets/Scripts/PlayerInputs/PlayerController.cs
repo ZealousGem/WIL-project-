5403e1a59b8f640ef626271af1e759c922cd6c09
Jordan Oos <141826168+ZealousGem@users.noreply.github.com>
2025-09-05 15:09:26 +0200

    fixed PlayerController Porblems

--- Assets/Scripts/PlayerInputs/PlayerController.cs
+++ Assets/Scripts/PlayerInputs/PlayerController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.AI;
 using Unity.VisualScripting;
+using System.Drawing;
+using UnityEditor.Analytics;
 
 
 public class PlayerController : MonoBehaviour
@@ -16,7 +18,9 @@ public class PlayerController : MonoBehaviour
 
     public float rotationSpeed;
     bool ReachedDestination = true;
-    
+
+    public float movementThreshold = 0.1f;
+
 
     float currentSpeed = 0;
 
@@ -35,13 +39,14 @@ public class PlayerController : MonoBehaviour
         cam = data.cam.GetComponent<Camera>();
     }
 
-   
+
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>(); 
-       // cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        agent = GetComponent<NavMeshAgent>();
+        // cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         animator = GetComponentInChildren<Animator>();
+        agent.updateRotation = false;
     }
 
     // Update is called once per frame
@@ -49,27 +54,29 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
         AnimateCharacter();
-        //Rotate();
+        Rotate();
 
 
     }
 
     void AnimateCharacter()
     {
-        if (!ReachedDestination)
+        if (agent.velocity.magnitude > 0.1f)
         {
-            currentSpeed = agent.velocity.magnitude;
-            animator.SetFloat("Speed", currentSpeed);
-           // Debug.Log(ReachedDestination);
+
+            // Debug.Log(ReachedDestination);
+            ReachedDestination = false;
         }
 
         else
         {
-            currentSpeed = 0;
-            animator.SetFloat("Speed", currentSpeed);
-         //   Debug.Log(ReachedDestination);
+            ReachedDestination = true;
+            //   Debug.Log(ReachedDestination);
         }
         
+         currentSpeed = agent.velocity.magnitude;
+            animator.SetFloat("Speed", currentSpeed);
+
     }
 
     void Movement()
@@ -82,15 +89,35 @@ public class PlayerController : MonoBehaviour
             {
                 if (hit.collider.gameObject)
                 {
-                    ReachedDestination = false;
-                    agent.destination = hit.point;
-                    if (agent.destination == hit.point)
-                    {
-                        ReachedDestination = true;
+                    NavMeshHit hitted;
+
+                    if (NavMesh.SamplePosition(hit.point, out hitted, 1.0f, NavMesh.AllAreas)) {
+                         agent.destination = hitted.position;
                     }
+                   
+                   
 
                     //  gameObject.transform.LookAt(movepls);
                 }
+            }
+        }
+    }
+    
+    void Rotate()
+    {
+        // Only rotate if the agent has a path and is not at the destination
+        if (agent.velocity.magnitude > movementThreshold)
+        {
+            // Get the direction to the next steering point
+            Vector3 direction = agent.steeringTarget - transform.position;
+            direction.y = 0; // Keep rotation on the horizontal plane
+
+            if (direction != Vector3.zero)
+            {
+                // Calculate the rotation needed to look at the steering target
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                // Smoothly rotate the character towards that direction
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             }
         }
     }
